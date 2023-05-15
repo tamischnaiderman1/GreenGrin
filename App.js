9eaf7437fb1457733b840cf5044ac65a7b1cad5b
tamischnaiderman1 <127223532+tamischnaiderman1@users.noreply.github.com>
2023-05-15 12:13:21 -0300

    mensaje bien!

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Puto el que lee</Text>
+      <Text>Hicimos nuestra primer app!!!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
